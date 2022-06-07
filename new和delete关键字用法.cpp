d6171f710970ffadced1b16a2c1b910f1fa6ac35
yt <y991749486@gmail.com>
2022-06-07 16:19:57 +0800

    练习new和delete

--- new和delete关键字用法.cpp
+++ new和delete关键字用法.cpp
@@ -1,1 +1,20 @@
 #include<iostream>
+
+using namespace std;
+
+int main() {
+
+	int * p = new int(10);	//new关键字创建变量
+	int* arr = new int[10];	//new关键字创建数组
+	for (int i = 0; i < 10; i++) {
+		*(arr + i) = i;
+	}
+	cout << *p << endl;
+
+	cout << arr[7] << endl;
+
+	delete p;		//释放单个变量空间
+	delete[] arr;	//释放数组空间，注意添加[]表示要释放的是一个数组
+	system("pause");
+	return 0;
+}
